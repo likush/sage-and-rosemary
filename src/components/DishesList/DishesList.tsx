c7fe119c2a109fa278155b86c0527b335fefcb1b
Lilia <fjordfield@gmail.com>
2024-05-30 11:13:28 +0200

    feat: refactor project structure

---
+++ src/components/DishesList/DishesList.tsx
@@ -0,0 +1,54 @@
+'use client'; // This is a client component 👈🏽
+
+import React, {useEffect} from 'react';
+import DishItem from '../DishItem/DishItem';
+import styles from './DishesList.module.scss';
+import {useDispatch, useSelector} from '@/redux/store';
+import {getDocs} from '@/services/firebase/firestore';
+
+const DishesList = (): React.JSX.Element => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getDocs());
+    }, [dispatch]);
+
+    const { dishes } = useSelector((state) => state.dishes);
+
+    // const getData = async (): Promise<void> => {
+    //     try {
+    //         let dishes: DishesType;
+    //
+    //         const staleTime = localStorage.getItem('staleTime');
+    //         const now = Date.now();
+    //
+    //         if (!staleTime || +staleTime < now) {
+    //             dishes = await getDocs();
+    //
+    //             const date = new Date()
+    //
+    //             localStorage.setItem('staleTime', `${date.setDate(date.getDate() + 1)}`);
+    //             localStorage.setItem('dishes', JSON.stringify(dishes));
+    //         } else {
+    //             const data = localStorage.getItem('dishes')
+    //             dishes = data ? JSON.parse(data) : []
+    //         }
+    //
+    //         setDishes(dishes)
+    //     } catch (err) {
+    //         console.log(err)
+    //     }
+    // }
+
+
+    return (
+        <div className={styles.dishesContainer}>
+            {
+                dishes.length > 0 &&
+                dishes.map((category, index) => <DishItem {...category} key={index}/>)
+            }
+        </div>
+    )
+}
+
+export default DishesList;
